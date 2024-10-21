7b5b673751c132aaf61329da73decb241347dbc5
Criolloc76 <94486186+Criolloc76@users.noreply.github.com>
2024-10-21 17:36:09 -0500

    Update script.js
    Signed-off-by: Criolloc76 <94486186+Criolloc76@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    const formData = new FormData(this);
-    fetch("https://script.google.com/macros/s/AKfycbzWJ48_4j_yvuatbyYf_Rs5qy9DecuDvalCGAvaGnJqSWDdegZSfbUarF3Uhe9C1TgD4A/exec", {
-        method: "POST",
-        body: formData
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById('registroForm');
+
+  form.addEventListener('submit', function(e) {
+    e.preventDefault(); // Evita que se recargue la página
+
+    const data = new FormData(form); // Crea un objeto FormData con los datos del formulario
+
+    // Envía los datos al script de Google Apps Script mediante fetch
+    fetch('https://script.google.com/macros/s/AKfycbzWJ48_4j_yvuatbyYf_Rs5qy9DecuDvalCGAvaGnJqSWDdegZSfbUarF3Uhe9C1TgD4A/exec', {
+      method: 'POST',
+      body: data
     })
-    .then(response => response.text())
+    .then(response => response.json())
     .then(data => {
-        alert(data);
-        this.reset(); // Limpia el formulario después de enviar
+      alert('Datos enviados correctamente');
+      form.reset(); // Limpia el formulario después de enviar los datos
     })
     .catch(error => {
-        console.error('Error:', error);
+      console.error('Error:', error);
+      alert('Hubo un error al enviar los datos');
     });
+  });
 });
