ee9a9258c27624a62e9f6bce05020e4f73a02de2
Criolloc76 <94486186+Criolloc76@users.noreply.github.com>
2024-10-20 20:15:33 -0500

    Update script.js
    Signed-off-by: Criolloc76 <94486186+Criolloc76@users.noreply.github.com>

--- script.js
+++ script.js
@@ -2,21 +2,23 @@ document.getElementById('lead-form').addEventListener('submit', function(event) 
     event.preventDefault(); // Evitar el envío normal del formulario
 
     // Obtener datos del formulario
+    var id = document.getElementById('id').value;
     var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
     var country = document.getElementById('country-select').value;
+    var email = document.getElementById('email').value;
     var phone = document.getElementById('phone-number').value;
 
     // Crear objeto de datos a enviar
     var data = {
-        name: name,
+        id: id,
+        nombre: name,
+        pais: country,
         email: email,
-        country: country,
-        phone: phone
+        telefono: phone
     };
 
     // Hacer la solicitud fetch a la API del script
-    fetch('https://script.google.com/macros/s/AKfycbw3h2___Z_wWrnDiNQjKpmlSv6tAHWhkLC9a7F26B-woo5zdODDZk9Jq4K-7XSTAd9H/exec', {
+    fetch('https://script.google.com/macros/s/AKfycbzWJ48_4j_yvuatbyYf_Rs5qy9DecuDvalCGAvaGnJqSWDdegZSfbUarF3Uhe9C1TgD4A/exec', {
         method: 'POST',
         body: JSON.stringify(data),
         headers: {
@@ -26,7 +28,7 @@ document.getElementById('lead-form').addEventListener('submit', function(event) 
     .then(response => response.json())
     .then(result => {
         // Mostrar el resultado en el formulario
-        document.getElementById('form-response').innerText = result.message;
+        document.getElementById('form-response').innerText = result.message || 'Datos enviados correctamente.';
         // Limpiar el formulario
         document.getElementById('lead-form').reset();
     })
